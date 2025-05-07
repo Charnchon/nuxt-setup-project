b2bb6da43315cf71bf74bcd29f36d1efb0b1397e
Charnchon Subsiri <charnchonsubsiri@Charnchons-MacBook-Air.local>
2025-05-07 22:28:22 +0700

    initialize project

---
+++ pages/stylesheet.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col gap-4 bg-gray-100 p-4">
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+  </div>
+</template>
