6469d6b4be99136b6fd8c10dcffef91fe3d51d34
charnchon <boatcharnchon@gmail.com>
2025-05-07 22:39:58 +0700

    FIX error code

--- pages/stylesheet.vue
+++ pages/stylesheet.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="flex flex-col gap-4 bg-gray-100 p-4">
+  <div class="min-h-screen flex flex-col gap-4 bg-gray-100 p-4">
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
+    <v-card text="..."></v-card>
     <v-card text="..."></v-card>
     <v-card text="..."></v-card>
     <v-card text="..."></v-card>
